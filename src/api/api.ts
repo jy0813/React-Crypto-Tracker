630ee3f80833ea320bad44b04e88eb33a66ba5e8
jy0813 <busbug3@gmail.com>
2023-02-21 20:24:59 +0900

    feat: Chart 연결

--- src/api/api.ts
+++ src/api/api.ts
@@ -13,3 +13,7 @@ export const axiosInfoData = async (coinId:string) => {
 export const axiosPriceData = async (coinId:string) => {
   return await axios.get(`${BASE_URL}/tickers/${coinId}`).then(res => res.data);
 }
+
+export const axiosCoinHistory = async (coinId:string) => {
+  return await axios.get(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`).then(res => res.data);
+}
--- src/page/Chart.tsx
+++ src/page/Chart.tsx
@@ -1,8 +1,69 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
+import {useQuery} from "react-query";
+import {axiosCoinHistory} from "../api/api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 function Chart() {
+  const {coinId} = useParams();
+  const {isLoading, data} = useQuery<IHistorical[]>(['ohlcv', coinId], () => axiosCoinHistory(coinId!))
   return (
-      <div>Chart</div>
+      <div>
+        {isLoading ? "Loading chart..." :
+            (
+                <ApexChart
+                    type='line'
+                    series={[
+                      {
+                        name: "Price",
+                        data:data?.map((price) => price.close) as number[],
+                      },
+                    ]}
+                    options={{
+                      theme:{
+                        mode:'dark'
+                      },
+                      chart:{
+                        height:500,
+                        width:500,
+                        background:'transparant',
+                        toolbar:{
+                          show:false,
+                        }
+                      },
+                      grid:{
+                        show:false
+                      },
+                      stroke:{
+                        curve:'smooth',
+                        width:4,
+                      },
+                      yaxis:{
+                        show:false
+                      },
+                      xaxis:{
+                        axisBorder:{
+                          show:false
+                        },
+                        axisTicks:{
+                          show:false,
+                        },
+                      }
+                    }}
+                />
+            )}
+      </div>
   );
 }
 
